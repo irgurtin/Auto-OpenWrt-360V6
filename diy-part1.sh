bb97a465e6244c667675173fbb90a7fc4b4b5620
Kangurei <59930019+Kangurei@users.noreply.github.com>
2022-09-09 11:52:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,10 +18,3 @@
 
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-
-
-git clone https://github.com/tty228/luci-app-serverchan.git                                              package/luci-app-serverchan
-git clone https://github.com/jerrykuku/luci-app-argon-config.git                                         package/luci-app-argon-config

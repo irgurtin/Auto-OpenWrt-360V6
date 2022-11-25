ae7e656556331f5968e372fd3c042efd1160cd75
irgurtin <59930019+irgurtin@users.noreply.github.com>
2022-11-25 09:25:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 
-# git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
+git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
 # git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/smartdns package/smartdns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+# git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
